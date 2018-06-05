53f051bd1f1175bb521d028f1c53c00582e6f78c
ZheRain <tone1024@163.com>
2018-06-05 17:55:38 +0800

    配置日志

--- src/main/java/com/zouj/test/App.java
+++ src/main/java/com/zouj/test/App.java
@@ -1,13 +1,19 @@
 package com.zouj.test;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.debug("asdfasdf89898989adsfasfd");
+        //System.out.println( "Hello World!" );
     }
 }
